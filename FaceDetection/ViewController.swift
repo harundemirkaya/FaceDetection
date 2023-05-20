fd4e62d26053d283e7d4ed470cb9d6495ff4949c
harundemirkaya <harundmrk@gmail.com>
2023-05-20 13:52:51 +0300

    Initial Commit

---
+++ FaceDetection/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FaceDetection
+//
+//  Created by Harun Demirkaya on 20.05.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
